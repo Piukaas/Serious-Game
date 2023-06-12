4dcd9a0680f5fadbba8bc4de615fac7229061b42
Thijs <thijsvanrijsbergen@gmail.com>
2023-06-12 14:06:39 +0200

    text update on start

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Heart : MonoBehaviour
 {
     private SceneManager sceneManager;
+    public Text titleText;
 
     private void Awake()
     {
         sceneManager = FindObjectOfType<SceneManager>();
+        if (PlayerPrefs.GetString("Character") == "Floor")
+        {
+            titleText.text = "Het leven in handen       Floor";
+        }
+        else if (PlayerPrefs.GetString("Character") == "Mark")
+        {
+            titleText.text = "Het leven in handen       Mark";
+        }
+        else if (PlayerPrefs.GetString("Character") == "Emma")
+        {
+            titleText.text = "Het leven in handen       Emma";
+        }
+        else if (PlayerPrefs.GetString("Character") == "Finn")
+        {
+            titleText.text = "Het leven in handen       Finn";
+        }
     }
 
     public void OnMouseDown()
